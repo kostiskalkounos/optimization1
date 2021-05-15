a2fd31299518837b6ff51c468a28aa7a16a3a160
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-15 16:39:37 +0200

    use React.memo()
    To check if props of a component needs to be re-rendered is props
    changed. Useful for big apps where entire branches can be cutoff from
    re-rendering needlessly.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,5 @@
+const MyParagraph = (props) => {
+  return <p>{props.show ? "This is new!" : ""}</p>;
+};
+
+export default MyParagraph;
