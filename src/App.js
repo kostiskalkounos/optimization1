37b122ca88894fe2f656857e4631c0fc2908010b
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-15 16:55:38 +0200

    use useCallback hook
    Store a function object in memory so it can be reused.
    In this way, React.memo() can be used with non primitive data types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -6,9 +6,9 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
